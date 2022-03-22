e16544f93887d2558f781ab0a2fd4cf32ef1fb0f
smogork <krzysztof.smogor@wp.pl>
2022-03-22 12:39:11 +0100

    Przeprojektowanie interfejsu

--- SmoCAD_en_GB.ts
+++ SmoCAD_en_GB.ts
@@ -5,145 +5,141 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="23"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="484"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="414"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="49"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="488"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="418"/>
         <source>Cursor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="57"/>
-        <location filename="mainwindow.ui" line="400"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="489"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="506"/>
+        <location filename="mainwindow.ui" line="118"/>
+        <location filename="mainwindow.ui" line="233"/>
+        <location filename="mainwindow.ui" line="290"/>
+        <location filename="mainwindow.ui" line="306"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="419"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="424"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="431"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="434"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="435"/>
+        <source>Y </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="86"/>
+        <location filename="mainwindow.ui" line="195"/>
+        <location filename="mainwindow.ui" line="211"/>
+        <location filename="mainwindow.ui" line="328"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="422"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="429"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="430"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="437"/>
+        <source>Z </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="134"/>
+        <location filename="mainwindow.ui" line="347"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="425"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="438"/>
         <source>Position</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="90"/>
-        <location filename="mainwindow.ui" line="258"/>
-        <location filename="mainwindow.ui" line="350"/>
-        <location filename="mainwindow.ui" line="370"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="490"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="500"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="503"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="505"/>
-        <source>Z</source>
+        <location filename="mainwindow.ui" line="70"/>
+        <location filename="mainwindow.ui" line="102"/>
+        <location filename="mainwindow.ui" line="167"/>
+        <location filename="mainwindow.ui" line="255"/>
+        <location filename="mainwindow.ui" line="271"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="421"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="423"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="428"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="432"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="433"/>
+        <source>X </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="100"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="491"/>
+        <location filename="mainwindow.ui" line="382"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="441"/>
+        <source>UV Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="406"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="442"/>
+        <source>V</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="439"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="443"/>
+        <source>U</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="462"/>
+        <location filename="mainwindow.ui" line="472"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="444"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="445"/>
+        <source>Density</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="144"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="426"/>
         <source>Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="107"/>
-        <location filename="mainwindow.ui" line="179"/>
-        <location filename="mainwindow.ui" line="202"/>
-        <location filename="mainwindow.ui" line="278"/>
-        <location filename="mainwindow.ui" line="407"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="492"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="495"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="497"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="501"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="507"/>
-        <source>X</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="117"/>
-        <location filename="mainwindow.ui" line="153"/>
-        <location filename="mainwindow.ui" line="288"/>
-        <location filename="mainwindow.ui" line="360"/>
-        <location filename="mainwindow.ui" line="417"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="493"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="494"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="502"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="504"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="508"/>
-        <source>Y</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="194"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="496"/>
+        <location filename="mainwindow.ui" line="159"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="427"/>
         <source>Transform</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="212"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="498"/>
+        <location filename="mainwindow.ui" line="357"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="439"/>
         <source>Rotation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="219"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="499"/>
+        <location filename="mainwindow.ui" line="367"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="440"/>
         <source>Scale</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="432"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="509"/>
-        <source>Torus parameters</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="440"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="510"/>
-        <source>r density</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="450"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="511"/>
-        <source>R</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="460"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="512"/>
-        <source>r</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="522"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="513"/>
-        <source>R density</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="562"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="514"/>
+        <location filename="mainwindow.ui" line="528"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="446"/>
         <source>Objects</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="566"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="515"/>
+        <location filename="mainwindow.ui" line="532"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="447"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="579"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="485"/>
+        <location filename="mainwindow.ui" line="545"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="415"/>
         <source>Torus</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="584"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="486"/>
+        <location filename="mainwindow.ui" line="550"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="416"/>
         <source>Point</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="589"/>
-        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="487"/>
+        <location filename="mainwindow.ui" line="555"/>
+        <location filename="cmake-build-debug/SmoCAD_autogen/include/ui_mainwindow.h" line="417"/>
         <source>Delete</source>
         <translation type="unfinished"></translation>
     </message>
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,17 +2,10 @@
 #include "./ui_mainwindow.h"
 #include "Objects/PointObject.h"
 
-/*Trzeba zrobić:
- * 2. Zastapic szescian torusem.
- * 3. Polaczyc kontrolki z parametrami torusa.
- */
-
 MainWindow::MainWindow(QWidget *parent)
         : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-
 }
 
 MainWindow::~MainWindow()
@@ -20,206 +13,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-/*void MainWindow::on_spinTorusR_valueChanged(double arg1)
-{
-    ui->sceneWidget->UpdateTorusObjectParameters(
-            arg1,
-            ui->sceneWidget->torus->GetSmallerR(),
-            ui->sceneWidget->torus->GetBiggerRDensity(),
-            ui->sceneWidget->torus->GetSmallerRDensity()
-    );
-}
-
-
-void MainWindow::on_spinTorusr_valueChanged(double arg1)
-{
-    ui->sceneWidget->UpdateTorusObjectParameters(
-            ui->sceneWidget->torus->GetBiggerR(),
-            arg1,
-            ui->sceneWidget->torus->GetBiggerRDensity(),
-            ui->sceneWidget->torus->GetSmallerRDensity()
-    );
-}
-
-
-void MainWindow::on_spinTorusRDensity_valueChanged(int arg1)
-{
-    ui->sceneWidget->UpdateTorusObjectParameters(
-            ui->sceneWidget->torus->GetBiggerR(),
-            ui->sceneWidget->torus->GetSmallerR(),
-            arg1,
-            ui->sceneWidget->torus->GetSmallerRDensity()
-    );
-}
-
-
-void MainWindow::on_spinTorusrDensity_valueChanged(int arg1)
-{
-    ui->sceneWidget->UpdateTorusObjectParameters(
-            ui->sceneWidget->torus->GetBiggerR(),
-            ui->sceneWidget->torus->GetSmallerR(),
-            ui->sceneWidget->torus->GetBiggerRDensity(),
-            arg1
-    );
-}
-
-
-void MainWindow::on_spinPosX_valueChanged(double arg1)
-{
-    QVector3D change(
-            arg1,
-            ui->sceneWidget->torus->Position.y(),
-            ui->sceneWidget->torus->Position.z()
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            change,
-            ui->sceneWidget->torus->Rotation,
-            ui->sceneWidget->torus->Scale
-    );
-}
-
-
-void MainWindow::on_spinPosY_valueChanged(double arg1)
-{
-    QVector3D change(
-            ui->sceneWidget->torus->Position.x(),
-            arg1,
-            ui->sceneWidget->torus->Position.z()
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            change,
-            ui->sceneWidget->torus->Rotation,
-            ui->sceneWidget->torus->Scale
-    );
-}
-
-
-void MainWindow::on_spinPosZ_valueChanged(double arg1)
-{
-    QVector3D change(
-            ui->sceneWidget->torus->Position.x(),
-            ui->sceneWidget->torus->Position.y(),
-            arg1
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            change,
-            ui->sceneWidget->torus->Rotation,
-            ui->sceneWidget->torus->Scale
-    );
-}
-
-
-void MainWindow::on_spinRotX_valueChanged(double arg1)
-{
-    QVector3D change(
-            arg1,
-            ui->sceneWidget->torus->Rotation.y(),
-            ui->sceneWidget->torus->Rotation.z()
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            ui->sceneWidget->torus->Position,
-            change,
-            ui->sceneWidget->torus->Scale
-    );
-}
-
-
-void MainWindow::on_spinRotY_valueChanged(double arg1)
-{
-    QVector3D change(
-            ui->sceneWidget->torus->Rotation.x(),
-            arg1,
-            ui->sceneWidget->torus->Rotation.z()
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            ui->sceneWidget->torus->Position,
-            change,
-            ui->sceneWidget->torus->Scale
-    );
-}
-
-
-void MainWindow::on_spinRotZ_valueChanged(double arg1)
-{
-    QVector3D change(
-            ui->sceneWidget->torus->Rotation.x(),
-            ui->sceneWidget->torus->Rotation.y(),
-            arg1
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            ui->sceneWidget->torus->Position,
-            change,
-            ui->sceneWidget->torus->Scale
-    );
-}
-
-
-void MainWindow::on_spinScaleX_valueChanged(double arg1)
-{
-    QVector3D change(
-            arg1,
-            ui->sceneWidget->torus->Scale.y(),
-            ui->sceneWidget->torus->Scale.z()
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            ui->sceneWidget->torus->Position,
-            ui->sceneWidget->torus->Rotation,
-            change
-    );
-}
-
-
-void MainWindow::on_spinScaleY_valueChanged(double arg1)
-{
-    QVector3D change(
-            ui->sceneWidget->torus->Scale.x(),
-            arg1,
-            ui->sceneWidget->torus->Scale.z()
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            ui->sceneWidget->torus->Position,
-            ui->sceneWidget->torus->Rotation,
-            change
-    );
-}
-
-
-void MainWindow::on_spinScaleZ_valueChanged(double arg1)
-{
-    QVector3D change(
-            ui->sceneWidget->torus->Scale.x(),
-            ui->sceneWidget->torus->Scale.y(),
-            arg1
-    );
-
-    ui->sceneWidget->UpdateTorusObjectTransform(
-            ui->sceneWidget->torus->Position,
-            ui->sceneWidget->torus->Rotation,
-            change
-    );
-}*/
-
-
 void MainWindow::on_actionTorus_triggered()
 {
     ui->sceneWidget->AddSelectableObject(new TorusObject(QVector3D(), 5, 1, 36, 18));
 }
 
-
 void MainWindow::on_actionPoint_triggered()
 {
     ui->sceneWidget->AddSelectableObject(new PointObject(QVector3D()));
 }
-
 
 void MainWindow::on_actionDelete_triggered()
 {
